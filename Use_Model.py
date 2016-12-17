63beb37303654d19353ae2a6364db052242bbd80
Thelordofdream <574483817@qq.com>
2016-12-17 22:16:17 +0800

    upload how to use model

--- Use_Model.py
+++ Use_Model.py
@@ -1,12 +1,20 @@
 import gensim
 
-model_dbow = gensim.models.Word2Vec.load('model_dbow')
-model_dm = gensim.models.Word2Vec.load('model_dm')
+# ===========================================
+# Doc2Vec
+model_dbow = gensim.models.Doc2Vec.load('model_dbow')
+model_dm = gensim.models.Doc2Vec.load('model_dm')
 # print model_dm.doesnt_match("breakfast cereal dinner lunch".split())
 # print model_dm['is']
 # print model_dbow['is']
-word1 = "is"
-word2 = "was"
+word1 = "huge"
+word2 = "large"
 print "The similarity between %s and %s is:" % (word1, word2)
 print model_dm.similarity(word1, word2)
-#print model_dbow.similarity('is', 'was')
+model_dbow.similarity('is', 'was')
+
+# ===========================================
+# Word2Vec
+model_w2v = gensim.models.Word2Vec.load('model_w2v')
+print "The similarity between %s and %s is:" % (word1, word2)
+print model_w2v.similarity(word1, word2)
