99b58b2ddba6b3207bfe1bc6d722d4d921e7ffb8
Thelordofdream <574483817@qq.com>
2016-12-17 21:03:37 +0800

    load train Doc2Vec

--- Use_Model.py
+++ Use_Model.py
@@ -5,5 +5,8 @@ model_dm = gensim.models.Word2Vec.load('model_dm')
 # print model_dm.doesnt_match("breakfast cereal dinner lunch".split())
 # print model_dm['is']
 # print model_dbow['is']
-print model_dm.similarity('is', 'was')
+word1 = "is"
+word2 = "was"
+print "The similarity between %s and %s is:" % (word1, word2)
+print model_dm.similarity(word1, word2)
 #print model_dbow.similarity('is', 'was')
