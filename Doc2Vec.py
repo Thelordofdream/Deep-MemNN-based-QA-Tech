890a68a53ff7a7e5e3a3ad6fab0d24068bb19332
Thelordofdream <574483817@qq.com>
2016-12-17 22:01:08 +0800

    Standardize Doc2Vec

---
+++ Doc2Vec.py
@@ -0,0 +1,96 @@
+import random
+import numpy as np
+import gensim
+
+LabeledSentence = gensim.models.doc2vec.LabeledSentence
+
+# ===========================================
+def cleanText(corpus):
+    punctuation = """.,?!:;(){}[]"""
+    corpus = [z.lower().replace('\n', '') for z in corpus]
+    corpus = [z.replace('<br />', ' ') for z in corpus]
+
+    # treat punctuation as individual words
+    for c in punctuation:
+        corpus = [z.replace(c, ' ') for z in corpus]
+    corpus = [z.split() for z in corpus]
+    return corpus
+
+
+# Gensim's Doc2Vec implementation requires each document/paragraph to have a label associated with it.
+# We do this by using the LabeledSentence method. The format will be "TRAIN_i" or "TEST_i" where "i" is
+# a dummy index of the review.
+def labelizeReviews(reviews, label_type):
+    labelized = []
+    for i, v in enumerate(reviews):
+        label = '%s_%s' % (label_type, i)
+        labelized.append(LabeledSentence(v, [label]))
+    return labelized
+
+
+def getVecs(model, corpus, size):
+    vecs = []
+    for z in corpus:
+        vec = np.zeros(size).reshape((1, size))
+        count = 0.
+        for each in np.array(model[z.words]):
+            try:
+                vec += each.reshape((1, size))
+                count += 1.
+            except:
+                continue
+        if count != 0:
+            vec /= count
+        vecs.extend(vec)
+    return np.array(vecs)
+
+
+def storeVecs(input, filename):
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(input, fw)
+    fw.close()
+
+
+def buildtrainvecs(model_dm, model_dbow, sentences, size):
+    # We pass through the data set multiple times, shuffling the training reviews each time to improve accuracy.
+    # all_train_reviews = np.concatenate((x_train))
+    train_vecs_dm = getVecs(model_dm, sentences, size)
+    train_vecs_dbow = getVecs(model_dbow, sentences, size)
+    train_vecs = np.hstack((train_vecs_dm, train_vecs_dbow))
+    return train_vecs
+
+
+def Train_Doc2Vec(Sentences, model_dm, model_dbow):
+    # ===========================================
+    # Pretreatment
+    number = len(Sentences)
+    x_train = cleanText(Sentences[:int(number * 98 / 100)])
+    unsup_reviews = cleanText(Sentences[int(number * 98 / 100):])
+
+    x_train = labelizeReviews(x_train, 'TRAIN')
+    unsup_reviews = labelizeReviews(unsup_reviews, 'UNSUP')
+
+    # ===========================================
+    # build vocab over all reviews
+    temp = x_train[:]
+    temp.extend(unsup_reviews)
+    model_dm.build_vocab(temp)
+    model_dbow.build_vocab(temp)
+
+    # ===========================================
+    # Train Doc2Vec models
+    iteration = 10
+    print "+==== Training =====+"
+    for epoch in range(iteration):
+        # perm = np.random.permutation(all_train_reviews.shape[0])
+        print "+=== Iteration %d ===+" % epoch
+        random.shuffle(temp)
+        model_dm.train(temp)
+        model_dbow.train(temp)
+
+    # ===========================================
+    # Store model and result
+    model_dm.save("model_dm")
+    model_dbow.save("model_dbow")
+    return temp
